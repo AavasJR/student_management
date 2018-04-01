2567a9fac94a714099573617731f06b52569de5f
aavas <mailme_aavas@yahoo.com>
2018-04-01 08:17:52 +0545

    First commit

--- src/main/java/com/itntraining/studentmanagement/Student.java
+++ src/main/java/com/itntraining/studentmanagement/Student.java
@@ -4,6 +4,8 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -12,16 +14,24 @@ public class Student {
 	@Id					// This makes the studentId as primary key
 	@GeneratedValue		// For auto increment
 	@Column(name = "student_id")	
-	private Long studentId;
-	
+	private Long studentId;	
 	@Column(name = "first_name")
-	private String firstName;
-	
+	private String firstName;	
 	@Column(name = "last_name")
-	private String lastName;
-	
+	private String lastName;	
 	@Column( name = "address")
 	private String address;
+	
+	@ManyToOne
+	@JoinColumn(name = "department_id")
+	private Department department;
+	
+	public Department getDeparatment() {
+		return department;
+	}
+	public void setDeparatment(Department deparatment) {
+		this.department = deparatment;
+	}
 	public Long getStudentId() {
 		return studentId;
 	}
